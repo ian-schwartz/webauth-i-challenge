e982e57af35705a4e80d77375c0d3ec5f1b64e97
Ian Schwartz <ian.schwartz23@gmail.com>
2019-11-11 12:55:05 -0800

    Authentication Project

---
+++ auth/requires-auth-middleware.js
@@ -0,0 +1,25 @@
+const bcrypt = require('bcryptjs');
+
+const Users = require('../users/users-model');
+
+module.exports = (req, res, next) => {
+  let { username, password } = req.headers;
+  
+  if (username && password) {
+    Users.findBy({ username })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        next();
+      } else {
+        res.status(401).json({ message: 'You Shall Not Pass!' });
+      }
+    })
+   .catch(error => {
+      console.log('login error', error);
+        res.status(500).json({ error: 'ran into an error, please try later'});
+      });
+  } else {
+    res.status(400).json({ message: 'please provide credentials' })
+  }
+};
