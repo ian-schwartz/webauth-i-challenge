20c74ccd9a0cdd8b5acba3d221c04481b4c6fc19
Ian Schwartz <ian.schwartz23@gmail.com>
2019-11-12 15:45:51 -0800

    sessions and cookies

--- api/server.js
+++ api/server.js
@@ -1,12 +1,40 @@
 const express = require('express');
+const session = require('express-session');
+const KnexSessionStorage = require('connect-session-knex')(session);
 
 const apiRouter = require('./api-router.js');
 const configMiddleware = require('./config-middleware');
+const knexConnection = require('../database/db-config');
 
 const server = express();
 
+const sessionConfig = {
+    name: 'tiger', // default name is sid
+    secret: process.env.COOKIE_SECRET || 'is it secret? is it safe?',
+    cookie: {
+      maxAge: 1000 * 6 * 60, // valid for 1 hour (in milliseconds)
+      secure: process.env.NODE_ENV === 'development' ? false : true, // do we send cookie over https only
+      httpOnly: true, // prevent client javascript code from accessing the cookie
+    },
+    resave: false, // save sessions even when they have not changed
+    saveUninitialized: true, // read about it on the docs to respect GDPR
+    store: new KnexSessionStorage({
+      knex: knexConnection,
+      clearInterval: 1000 * 60 * 10, // delete expired sessions every 10 mins
+      tablename: 'user_sessions',
+      sidfieldname: 'id',
+      createtable: true
+    })
+  };
+
 configMiddleware(server);
 
+server.use(session(sessionConfig));
+
 server.use('/api', apiRouter);
+
+server.get('/', (req, res) => {
+    res.json({ api: 'up', session: req.session });
+  });
 
 module.exports = server;
--- auth/requires-auth-middleware.js
+++ auth/requires-auth-middleware.js
@@ -1,25 +1,33 @@
-const bcrypt = require('bcryptjs');
-
-const Users = require('../users/users-model');
-
 module.exports = (req, res, next) => {
-  let { username, password } = req.headers;
-  
-  if (username && password) {
-    Users.findBy({ username })
-    .first()
-    .then(user => {
-      if (user && bcrypt.compareSync(password, user.password)) {
-        next();
-      } else {
-        res.status(401).json({ message: 'You Shall Not Pass!' });
-      }
-    })
-   .catch(error => {
-      console.log('login error', error);
-        res.status(500).json({ error: 'ran into an error, please try later'});
-      });
+  if (req.session && req.session.username) {
+    next();
   } else {
-    res.status(400).json({ message: 'please provide credentials' })
+    res.status(401).json({ message: 'You cannot pass!' });
   }
 };
+
+// const bcrypt = require('bcryptjs');
+
+// const Users = require('../users/users-model');
+
+// module.exports = (req, res, next) => {
+//   let { username, password } = req.headers;
+  
+//   if (username && password) {
+//     Users.findBy({ username })
+//     .first()
+//     .then(user => {
+//       if (user && bcrypt.compareSync(password, user.password)) {
+//         next();
+//       } else {
+//         res.status(401).json({ message: 'You Shall Not Pass!' });
+//       }
+//     })
+//    .catch(error => {
+//       console.log('login error', error);
+//         res.status(500).json({ error: 'ran into an error, please try later'});
+//       });
+//   } else {
+//     res.status(400).json({ message: 'please provide credentials' })
+//   }
+// };
